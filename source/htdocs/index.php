953d768e10699d1f4b92377bf9a37af1eb9c5146
Volker Dusch <github@wallbash.com>
2012-01-13 20:07:30 +0100

    Forgot the index.php - Now starting with SF components

---
+++ source/htdocs/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$input = isset($_GET['name']) ? $_GET['name'] : 'World';
+
+header('Content-Type: text/html; charset=utf-8');
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+
