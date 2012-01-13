62f5f540515dafa913ed8ad186607578d1474f18
Volker Dusch <github@wallbash.com>
2012-01-13 20:23:05 +0100

    Fixed copy paste issue

--- source/htdocs/index.php
+++ source/htdocs/index.php
@@ -12,8 +12,4 @@ $input = $request->get('name', 'World');
 $response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
  
 $response->send();
-$input = isset($_GET['name']) ? $_GET['name'] : 'World';
-
-header('Content-Type: text/html; charset=utf-8');
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
 
