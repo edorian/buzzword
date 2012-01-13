1c1b0e62e3fe1b09c71a83cd53b2f8f980705b4f
Volker Dusch <github@wallbash.com>
2012-01-13 20:04:00 +0100

    Put in the next dependency into the autoloader

--- source/sf_autoload.php
+++ source/sf_autoload.php
@@ -8,4 +8,8 @@ require_once __DIR__.'/../vendor/symfony/class-loader/Symfony/Component/ClassLoa
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 $loader = new UniversalClassLoader();
 $loader->register();
+
+// Do i need to this every time I add something to vendor?
+$loader->registerNamespace('Symfony\\Component\\HttpFoundation', __DIR__.'/vendor/symfony/http-foundation');
+ 
  
