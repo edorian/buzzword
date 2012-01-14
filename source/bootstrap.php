67a70f98f73cc5c7f2af016f0518b247c33279ca
Volker Dusch <github@wallbash.com>
2012-01-14 02:48:26 +0100

    Use autogenerated autoloader

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
-require __DIR__ . '/sf_autoload.php';
+// It feels quite strange to use that autoload scaffoling that is not in version control..
+// I wonder how deployments of this work.. well packing up a working build includes this so all is fine :)
+require_once __DIR__.'/../vendor/.composer/autoload.php';
