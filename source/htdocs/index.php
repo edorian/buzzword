ebfda8d05727b8dc969e2b61906bce39b991e1d0
Volker Dusch <github@wallbash.com>
2012-01-16 11:12:10 +0100

    Extract template renderer

--- source/htdocs/index.php
+++ source/htdocs/index.php
@@ -4,42 +4,44 @@ require_once __DIR__ . '/../bootstrap.php';
  
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing;
 
 // Ugly hack to keep the tests for now
 if(!isset($request)) {
     $request = Request::createFromGlobals();
 }
+
+// Hack to keep the tests going..
+if(!function_exists('render_template')) { 
+    function render_template($request) {
+        extract($request->attributes->all(), EXTR_SKIP);
+        ob_start();
+        require sprintf(__DIR__ . '/../pages/%s.php', $_route);
+        return new Response(ob_get_clean());
+    }
+}
+
 $response = new Response();
 
 $pages = __DIR__ . '/../pages/';
 
-$routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Route('/bye'));
+$routes = new Routing\RouteCollection();
+$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
+$routes->add('bye', new Routing\Route('/bye'));
 
-$context = new RequestContext();
+$context = new Routing\RequestContext();
 $context->fromRequest($request);
-$matcher = new UrlMatcher($routes, $context);
-
-$response = new Response();
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    require sprintf(__DIR__ . '/../pages/%s.php', $_route);
-    $response->setContent(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = render_template($request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
-} catch (\Exception $e) {
-    $response->setStatusCode(500);
-    $response->setContent('Internal Server error');
-    var_dump($e->getMessage());
+    $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred: ' . $e->getMessage(), 500);
 }
+
  
 $response->send();
 
