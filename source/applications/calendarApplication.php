4e1ea85f7f0d07adde1a3cc2c18b2185819a11b2
Volker Dusch <github@wallbash.com>
2012-01-17 17:51:41 +0100

    Rename LeapYearApplication to CalendarApplication step 1

--- source/applications/calendarApplication.php
+++ source/applications/calendarApplication.php
@@ -5,8 +5,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 // Manual loaind for now
 // There will be an application specific bootstrap when there is a proply seperated application
-require_once __DIR__ . '/leapYearApplication/LeapYearController.php';
-require_once __DIR__ . '/leapYearApplication/LeapYearCalculator.php';
+require_once __DIR__ . '/calendarApplication/LeapYearController.php';
+require_once __DIR__ . '/calendarApplication/LeapYearCalculator.php';
 
 $routes = new Routing\RouteCollection();
 $routes->add(
